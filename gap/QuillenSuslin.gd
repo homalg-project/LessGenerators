d245981e8ea9a50c647c161cd3b5daa3623ae6bb
VInay Wagh <vinay_wagh@yahoo.com>
2015-07-05 20:28:07 +0200

    Minor corrections in the documentaion of QuillenSuslin

--- gap/QuillenSuslin.gd
+++ gap/QuillenSuslin.gd
@@ -36,10 +36,10 @@ DeclareOperation( "QuillenSuslinUnipotent",
                   [ IsHomalgMatrix ] );
 
 #! @Description
-#!  Compute for the <M>r \times c</M>-matrix <A>mat</A> a square matrix <M>V</M>
-#!  such that <A>mat</A> <M>* V</M> is equal to the first <M>r</M> rows
-#!  of the <M>c \times c</M> identity matrix.
+#!  For the given <M>r \times c</M>-matrix <A>mat</A>, compute a square 
+#!  matrix <M>V</M> such that <A>mat</A> <M>* V</M> is equal to the first 
+#!  <M>r</M> rows of the <M>c \times c</M> identity matrix.
 #! @Arguments mat
-#! @Returns a list of two &homalg; matrices
+#! @Returns a &homalg; matrix
 DeclareOperation( "QuillenSuslin",
                   [ IsHomalgMatrix ] );
